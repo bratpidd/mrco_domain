2c0b3dbc94231693abf5f4c34f532a1d6cc94402
MRCO <pts09a@ya.ru>
2017-08-16 06:13:11 +0300

    doomfist

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');1
+        //$this->middleware('auth');
     }
 
     /**
