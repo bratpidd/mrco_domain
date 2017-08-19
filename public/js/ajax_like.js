696ccde9f5da631ebd3f429111d86cb9bf0aa9a6
MRCO <pts09a@ya.ru>
2017-08-19 05:07:39 +0300

    doomfisted

--- public/js/ajax_like.js
+++ public/js/ajax_like.js
@@ -24,6 +24,7 @@ jQuery( document ).ready( function($) {
         //do anything else you might want to do
         //.....
         document.getElementById('submit').value = 'd';
+        //skiborg dolboeb
         //prevent the form from actually submitting in browser
         e.preventDefault();
         return false;
