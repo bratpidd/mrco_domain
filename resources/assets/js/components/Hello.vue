d5faca99c6f2304643e9648563f96969b1958f64
bratpidd <bratpidd2012@gmail.com>
2018-05-25 17:59:02 +0300

    doomfisted

--- resources/assets/js/components/Hello.vue
+++ resources/assets/js/components/Hello.vue
@@ -45,7 +45,7 @@
         name: 'hello',
         data () {
             return {
-                msg: 'doomfist to Your Vue.js App',
+                msg: 'doomfis to Your Vue.js App',
                 users: [
                     {firstname: 'Sebastian', lastname: 'Eschweiler'},
                     {firstname: 'Bill', lastname: 'Smith'},
