150b00361681d764429f5041359fbb93cc830ab2
bratpidd <30573879+bratpidd@users.noreply.github.com>
2020-01-10 20:16:37 +0300

    resurrecshon

--- resources/views/newpost.blade.php
+++ resources/views/newpost.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">New Post</div>
+                <div class="panel-heading">Newo Post</div>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('new_post_submit') }}">
                         {{ csrf_field() }}
