7df380ec439887158f8d22f3a611d44a2cae9fe3
bratpidd <30573879+bratpidd@users.noreply.github.com>
2020-01-10 20:29:41 +0300

    resurrecshon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li><a href="{{ route('vue_about') }}">New Post1</a></li>
+                            <li><a href="{{ route('vue_about') }}">New Post</a></li>
                             <li><a href="{{ route('sub') }}">My Subscriptions</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
