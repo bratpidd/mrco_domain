a963e747c7af65086d747d98b015b8112d873d02
bratpidd <30573879+bratpidd@users.noreply.github.com>
2020-01-14 22:23:35 +0300

    updated

--- resources/views/testpage.php
+++ resources/views/testpage.php
@@ -11,7 +11,24 @@ echo '$var';
 echo $var;
 echo("$var");
 print($var);
+
+
+$ar = array(
+    array(
+        'name' => 'Иванов',
+        'specialty' => 'хирург'
+    ),
+    array (
+            'name' => 'Петров',
+            'specialty' => 'кардиолог'
+    )
+);
+function noIvanov($array){
+    $response = [];
+    foreach ($array as $v){
+        if ($v['name'] !== 'Иванов') {$response[] = $v;}
+    }
+    return $response;
+}
+echo (noIvanov($ar));
 ?>
-<script>setTimeout(function () {
-    alert("done");
-}, 3000);</script>
